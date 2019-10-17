375fa2233c9d0a3f2db367054c5f72480017cca8
vonaxl <vonaxlcorso@gmail.com>
2019-10-17 15:06:40 +0200

    js done

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,28 @@
 // chiedi il numero di chilometri e eta
-var chilometri = prompt("Quanti chilometri?");
-var eta = prompt ("Quanti anni hai?");
+var chilometri = parseFloat(prompt("Quanti chilometri?"));
+console.log("sono "+chilometri+" km");
+var eta = parseInt(prompt ("Quanti anni hai?"));
+console.log("Hai "+eta+" anni");
+var prezzoB;
+// calcolo del prezzo biglietto in base ai km(0.21cebt x 1km)
+// console.log("Il prezzo del biglietto è €"+prezzoB);
+// condizione se eta è sotto 18 anni allora -20%
+if (eta>=65) {
+  prezzoB = ((0.21 * chilometri) * .40);
+  var decimal = prezzoB.toFixed(2);
+  console.log("Il prezzo con sconto senior è €"+decimal);
+}
+else if(eta<=17) {
+  prezzoB = ((0.21 * chilometri) * .20);
+  decimal = prezzoB.toFixed(2);
+  console.log("Il prezzo con sconto junior è €"+decimal);
+}
+else {
+  prezzoB = (0.21 * chilometri);
+  decimal = prezzoB.toFixed(2);
+  console.log("Il prezzo del biglietto è €"+decimal);
+}
+
+// condizione se eta è soppra 65 anni allora -40%
+
+// calcolo prezzo totale del'meta
